8a802db5d41bd652fe2742aa6f4be8073d756ff1
handsomevang <legithandsomevang@gmail.com>
2021-05-22 05:44:02 -0400

    addedmore css

--- models/entry.js
+++ models/entry.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const entrySchema = mongoose.Schema({
   text: String,
-  imageUrl: String
+  img: { data: Buffer, contentType: String }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Entry', entrySchema);
